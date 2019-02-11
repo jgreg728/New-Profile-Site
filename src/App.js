a4c27561574a1acbc6bfc4390f4a52fe82f62689
jgreg728 <jjg070890@aol.com>
2019-02-11 15:27:20 -0500

    added css files to components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Contact from "./components/Contact";
+import Portfolio from "./components/Portfolio";
+import Footer from "./components/Footer";
+import './App.css'
 
 class App extends Component {
   render() {
