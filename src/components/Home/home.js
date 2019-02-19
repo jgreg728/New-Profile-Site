2679753593ab10d3a6367ac2d42a0e0a8c1c23f6
jgreg728 <jjg070890@aol.com>
2019-02-19 00:22:37 -0500

    saving

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -2,15 +2,16 @@ import React from "react";
 import Header from "./header.js";
 import Footer from "./footer.js";
 import "./home.css";
+import Wrapper from "../Wrapper/wrapper.js";
 
 const Home = (props) => {
   return (
-  
-    <div>
-      <h1>John Gregorio's Offical Site</h1>
+    <Wrapper>
+      <div>
+       <h1>John Gregorio's Offical Site</h1>
       <img></img>
     </div>
-   
+    </Wrapper>
   )
   
 }
