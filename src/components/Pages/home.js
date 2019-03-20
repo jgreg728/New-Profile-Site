70f8b67aa55ad99fb1038f8ee9d5783c79a6e87e
jgreg728 <jjg070890@aol.com>
2019-03-20 14:40:32 -0400

    saving routes

--- src/components/Pages/home.js
+++ src/components/Pages/home.js
@@ -14,7 +14,7 @@ const Home = (props) => {
         <div>
         <h1>John Gregorio's Offical Site</h1>
        
-       <img src={require('../assets/john_gregorio_profile_pic.png')} alt="John Gregorio"/>
+       <img src={require("../../assets/john_gregorio_profile_pic.png")} alt="John Gregorio"/>
         
         <h3>
         "A creative, forward-thinking and idea-driven worker, John is an experienced Marketing Specialist and a St. John’s University graduate with a Bachelor of Science (BS), focused in Marketing within the Peter J. Tobin College of Business. He built a career of working in the information technology and branding industries, combining his passion for consumer technology products with his passion for effective marketing strategies. Certified in full-stack coding from Rutgers' Coding Bootcamp Continuing Studies Program he is able to consistently visualize and build digital marketing concepts."
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Router, Route } from "react-router";
-import App from "./App"
+import Home from "../components/Pages/home"
+import About from "../components/Pages/about"
+import Contact from "../components/Pages/contact"
+import Portfolio from "../components/Pages/portfolio"
+import App from "../App"
 
 export default (
   <Router>
     <Route path="/" component={App}>
       <Route path="home" component={Home} />
       <Route path="about" component={About} />
+      <Route path="contact" component={Contact} />
+      <Route path="portfolio" component={Portfolio} />
     </Route>
   </Router>
 )
