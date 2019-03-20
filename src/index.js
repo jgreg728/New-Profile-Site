f13050ecbada831f1dba7c60c369dd6a0a2c3d98
jgreg728 <jjg070890@aol.com>
2019-03-20 14:15:29 -0400

    saving profile picture

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import router from "./routes/routes"
 
 ReactDOM.render(<App/>, document.getElementById('root'));
