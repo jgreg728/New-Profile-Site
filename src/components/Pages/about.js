63c497472e68d302a217f9f86a93c72f364fc768
jgreg728 <jjg070890@aol.com>
2019-05-03 22:01:14 -0400

    edited About section

--- src/components/Pages/about.js
+++ src/components/Pages/about.js
@@ -38,7 +38,7 @@ class About extends Component {
             <Col md={6}>
               <h2>
                 <p>
-                  Prior to his graduation, he was fortunate enough to enter an internship at Pershing, a financial services firm, working in their branding, advertising, and customer communications division.  It was here that I was involved in many projects and global meetings, including updating the design of the company’s website. In addition, during my senior year, one of my courses had me participate in a real marketing project that involved the development of a full marketing plan. This included collaboration with an external organization and an intermediate school, to help that school win a grant to help enhance its campus.
+                  Prior to his graduation, he was fortunate enough to enter an internship at Pershing, a financial services firm, working in their branding, advertising, and customer communications division.  It was here that he was involved in many projects and global meetings, including updating the design of the company’s website. In addition, during his senior year, one of his courses had him participate in a real marketing project that involved the development of a full marketing plan. This included collaboration with an external organization and an intermediate school, to help that school win a grant to help enhance its campus.
             </p>
               </h2>
             </Col>
@@ -54,7 +54,7 @@ class About extends Component {
             <Col>
               <h2>
                 <p>
-                  In July of 2018, John took up a bootcamp course for full-stack coding within Rutgers' continuing studies program. During the 6 months of the course, he became educated on the ins and outs of both front-end and back-end coding. Going through languages such as HTML, CSS, Javascript, jQuery, and React, John accured a portfolio of interesting and creative works from the assignments given, as you will see in the Portfolio page of this site. John  graduated the program in February 2019, certified in completing Rutgers Coding Bootcamp.
+                  In July of 2018, John took up a bootcamp course for full-stack coding within Rutgers' continuing studies program. During the 6 months of the course, he became educated on the ins and outs of both front-end and back-end coding. Going through languages such as HTML, CSS, Javascript, jQuery, and React, John accrued a portfolio of interesting and creative works from the assignments given, as you will see in the Portfolio page of this site. John  graduated the program in February 2019, certified in completing Rutgers Coding Bootcamp.
                </p>
               </h2>
             </Col>
